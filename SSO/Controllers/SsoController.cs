66b7e9e35658dd6298b82fafbb2373a644fbb071
Mateusz Puchalski <mateusz.puchalski@gmail.com>
2022-03-17 12:55:11 +0100

    init

---
+++ SSO/Controllers/SsoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SSO.Services;
+
+namespace SSO.Controllers
+{
+    public class SsoController : Controller
+    {
+        private IConfiguration _configuration;
+        private readonly SsoService _ssoService;
+
+        public SsoController(IConfiguration configuration, SsoService ssoService)
+        {
+            _configuration = configuration;
+            _ssoService = ssoService;
+        }
+        public IActionResult Commento(string token, string hmac)
+        {
+            string secretKey = _configuration.GetValue<string>("CommentoSecretKey");
+
+            var bHmac= Encoding.Default.GetBytes(hmac);
+            var expectedHmac = _ssoService.GenerateHmac(token, secretKey);
+            
+            if  (bHmac != expectedHmac)
+                return Forbid();
+
+
+            var payload = 
+                new { token = token, 
+                    email = HttpContext.User.Identity.Name, 
+                    name = HttpContext.User.Identity.Name };
+
+            var hmacPayload = _ssoService.GenerateHmac(Newtonsoft.Json.JsonConvert.SerializeObject(payload), secretKey);
+
+            var newHmac = BitConverter.ToString(hmacPayload).Replace("-", "");
+            var bPayload = Encoding.Default.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(payload));
+            var hexPayload = BitConverter.ToString(bPayload).Replace("-", "");
+
+            return Redirect("https://commento.io/api/oauth/sso/callback?payload=" + hexPayload + "&hmac=" + newHmac);
+        }
+
+        
+    }
+}
---
+++ SSO/Services/SsoService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SSO.Services
+{
+    public class SsoService
+    {
+        public Byte[] GenerateHmac(string text, string secretKey)
+        {
+            var encoding = new ASCIIEncoding();
+
+            var textBytes = Encoding.Default.GetBytes(text);
+            var keyBytes = Encoding.Default.GetBytes(secretKey);
+
+
+            using var hash = new HMACSHA256(keyBytes);
+            var hashBytes = hash.ComputeHash(textBytes);
+
+            return hashBytes;
+        }
+    }
+}
